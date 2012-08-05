65e54596b353cca2448adc5d89bd5047aab34492
Darrell Bailey <darrell@disruptiontheory.com>
2012-08-05 11:50:16 -0400

    Added the shared types implementation

---
+++ WebDETypes/Net/Resources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDETypes.Net
+{
+    public enum Resources
+    {
+        MAP,
+        SPRITE,
+        TILE
+    }
+}
