8f3a7393216a07454a0d9c20d7c3fedee27dacdf
Darrell Bailey <darrell@disruptiontheory.com>
2012-08-10 12:36:27 -0400

    Added UITK under tools. not yet fully implemented

---
+++ Tools/UITK/Surface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using SharpKit.JavaScript;
+using SharpKit.Html4;
+using SharpKit.jQuery;
+
+namespace UITK
+{
+    [JsType(JsMode.Clr, Filename = "scripts/UITK.js")]
+    public class Surface : jQueryContextBase
+    {
+        /// <summary>
+        /// The width of the viewable inner area of the browser window.
+        /// </summary>
+        public static int Width
+        {
+            get { return window.innerWidth; }
+        }
+
+        /// <summary>
+        /// The height of the viewable inner area of the broswer window.
+        /// </summary>
+        public static int Height
+        {
+            get { return window.innerHeight; }
+        }
+    }
+}
